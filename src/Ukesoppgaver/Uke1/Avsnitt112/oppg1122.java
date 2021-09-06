aab1635142df15b64899effa560efae15badad6e
ReyhanAZ <70692444+ReyhanAZ@users.noreply.github.com>
2021-09-06 12:23:11 +0200

    Initial commit

---
+++ src/Ukesoppgaver/Uke1/Avsnitt112/oppg1122.java
@@ -0,0 +1,19 @@
+package Ukesoppgaver.Uke1.Avsnitt112;
+
+public class oppg1122 {
+    public static int min(int[] a)  // a er en heltallstabell
+    {
+        if (a.length < 1)
+            throw new java.util.NoSuchElementException("Tabellen a er tom!");
+
+        int m = 0;  // indeks til minste verdi
+
+        for (int i = 1; i < a.length; i++) // obs: starter med i = 1
+        {
+            if (a[i] < a[m]) m = i;  // indeksen oppdateres
+        }
+
+        return m;  // returnerer indeksen/posisjonen til minste verdi
+
+    } // min
+}
