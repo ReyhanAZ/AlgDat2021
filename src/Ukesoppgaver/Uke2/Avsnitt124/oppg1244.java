0c725c3e00e92b8114ca99b3b46e4a7b5f1ee21a
ReyhanAZ <70692444+ReyhanAZ@users.noreply.github.com>
2021-09-14 23:23:43 +0200

    Oblig 1 - Oppgave 2 og 3

--- src/Ukesoppgaver/Uke2/Avsnitt124/oppg1244.java
+++ src/Ukesoppgaver/Uke2/Avsnitt124/oppg1244.java
@@ -10,4 +10,50 @@ public class oppg1244 {
             Tabell.bytt(a,i-1,m);
         }
     }
+
+    //Oblig 1
+
+    public static int antallUlikeSortert(int[] a){
+
+        int count = 0;
+        int value = 0;
+
+        if(a.length < 1){
+            return 0;
+        }
+
+        for (int i = 0; i < a.length; i++){
+
+            if (a[i] >= value){
+
+                if (a[i] != value){
+                    count++;
+                }
+                 value = a[i];
+            }
+            else {
+                throw new IllegalStateException("Arrayet er ikke sortert");
+            }
+        }
+        return count;
+    }
+
+    public static int antallUlikeUsortert(int[] a){
+
+        if(a.length < 1){
+            return 0;
+        }
+
+        int count = a.length;
+
+        for (int i = 0; i < a.length; i++){
+            for (int j = i+1; j < a.length; j++){
+                if(a[i] == a[j]){
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
 }
